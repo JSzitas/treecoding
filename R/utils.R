7d06cd0c1bc44beeb7e071d8e4a484a56824ff13
Juraj Szitas <szitas.juraj13@gmail.com>
2022-02-17 00:01:22 +0100

    Update utils so sampling does not fail with obscure errors

--- R/utils.R
+++ R/utils.R
@@ -11,14 +11,23 @@ resample_folds <- function( range = 10, size = 2 ) {
 }
 
 first <- function(x) {
+  if( length(x) == 0) {
+    return(NA)
+  }
   x[[1]]
 }
 
 last <- function(x) {
+  if( length(x) == 0) {
+    return(NA)
+  }
   x[[length(x)]]
 }
 
 random_sample <- function(x) {
+  if( length(x) == 0) {
+    return(NA)
+  }
   if( is.character(x) ) {
     return( sample( x, 1 ))
   }
@@ -26,8 +35,45 @@ random_sample <- function(x) {
 }
 
 mean_sample <- function(x) {
+  if( length(x) == 0) {
+    return(NA)
+  }
   if( is.character(x) ) {
-    return( sample( x, 1 ))
+      return( sample( x, 1 ))
   }
   return( mean(x) )
 }
+
+is_encoder_forest <- function( x ) {
+  class(x) == "encoder_forest"
+}
+
+#' @export
+`[.encoder_forest` <- function( x, j, drop = FALSE) {
+  if( !is_encoder_forest(x) ) {
+    return(NextMethod())
+  }
+  j_arg <- substitute(j)
+  if (missing(j)) {
+    j <- NULL
+    j_arg <- NULL
+  }
+  else if (is.null(j)) {
+    j <- integer()
+  }
+  if( drop ) {
+    return(unclass(x)[j])
+  }
+  structure( unclass(x)[j], class = "encoder_forest" )
+}
+#' @export
+`[[.encoder_forest` <- function (x, i, j, ..., exact = TRUE)
+{
+  NextMethod()
+}
+#' @export
+`$.encoder_forest` <- function(x, i) {
+  NextMethod()
+}
+
+
