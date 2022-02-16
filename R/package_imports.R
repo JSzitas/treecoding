8265940767977fee35a3d22bf43a8dbef6e2e544
Juraj Szitas <szitas.juraj13@gmail.com>
2022-02-17 00:10:24 +0100

    Update package imports

--- R/package_imports.R
+++ R/package_imports.R
@@ -2,6 +2,8 @@
 #' @importFrom furrr future_map
 #' @importFrom furrr furrr_options
 #' @importFrom stats setNames
+#' @importFrom purrr map_dfc
 #' @importFrom dplyr bind_rows
+#' @importFrom purrr map
 #' @importFrom furrr future_map2
 NULL
