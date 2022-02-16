ebaad1d4330f8aa851051f299ace35a2626ecfd5
Juraj Szitas <szitas.juraj13@gmail.com>
2022-02-17 00:00:58 +0100

    Update package formalities

--- experiments/test_script.R
+++ experiments/test_script.R
@@ -1,5 +1,28 @@
 
+remove(list=ls())
 
-X <- matrix(rnorm(500), ncol = 5)
-Y <- matrix(rnorm(100), ncol = 5)
+pkgload::load_all()
+
+n = 100000
+p = 6
+
+df <- data.frame( fact = sample(letters[1:6], n, replace = TRUE),
+                  matrix(rnorm(n*(p-1)), ncol = p-1))
+
+tree <- random_tree(df)
+encoded_tree <- encode( tree, df[1:100,] )
+
+decoded_tree <- decode( tree, encoded_tree )
+
+
+
+# future::plan("multisession")
+# future::plan("sequential")
+#
+# forest <- encoder_forest(df, n_tree = 1000, max_depth = 10, subsample_size = 100)
+# encoded_forest <- encode( forest, df[1:100,] )
+#
+# decoded_forest <- decode( forest, encoded_forest )
+
+
 
