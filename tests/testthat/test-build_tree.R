d1cb2b58d188cc3ea4184925cf6c345315cdc128
Juraj Szitas <szitas.juraj13@gmail.com>
2022-07-23 15:28:04 +0200

    Styling

--- tests/testthat/test-build_tree.R
+++ tests/testthat/test-build_tree.R
@@ -1,15 +1,16 @@
 test_that("Fitting a random tree works", {
-
   set.seed(1071)
 
-  n = 100000
-  p = 6
+  n <- 100000
+  p <- 6
 
-  df <- data.frame( fact = sample(letters[1:6], n, replace = TRUE),
-                    matrix(rnorm(n*(p-1)), ncol = p-1))
+  df <- data.frame(
+    fact = sample(letters[1:6], n, replace = TRUE),
+    matrix(rnorm(n * (p - 1)), ncol = p - 1)
+  )
   tree <- random_tree(df, max_depth = 8)
 
   expect_s3_class(tree, "random_tree")
-  expect_equal( tree[["rule"]][["column"]], 4 )
-  expect_equal( tree[["rule"]][["rule"]], 0.6484567, tolerance = 0.05 )
+  expect_equal(tree[["rule"]][["column"]], 4)
+  expect_equal(tree[["rule"]][["rule"]], 0.6484567, tolerance = 0.05)
 })
--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -3,10 +3,9 @@ test_objects <- readRDS("testdata/decoding_data.rda")
 forest <- test_objects$forest
 
 test_that("subsetting a forest works", {
-
-  expect_true( is_encoder_forest(forest))
-  expect_true( is_encoder_forest(forest[1]) )
-  expect_true( is_encoder_forest(forest[1:5]) )
-  expect_true( is_encoder_forest(forest[]) )
-  expect_true( is_random_tree( forest[10, drop = TRUE] ) )
+  expect_true(is_encoder_forest(forest))
+  expect_true(is_encoder_forest(forest[1]))
+  expect_true(is_encoder_forest(forest[1:5]))
+  expect_true(is_encoder_forest(forest[]))
+  expect_true(is_random_tree(forest[10, drop = TRUE]))
 })
